e192e46be5c519606b5c65f457fa2eac46cd5d28
benjamink <benjamink@mirageoscience.com>
2024-01-17 10:51:44 -0800

    adds filter_xy and elevates all the utilities to init so that they can be imported directly from geoapps_utils.  Tested with simpeg-drivers and no circular imports.

--- geoapps_utils/__init__.py
+++ geoapps_utils/__init__.py
@@ -8,3 +8,19 @@
 from __future__ import annotations
 
 __version__ = "0.3.0-alpha"
+
+from .conversions import hex_to_rgb, string_to_numeric
+from .formatters import string_name
+from .importing import warn_module_not_found, assets_path
+from .iterables import (
+    find_value, sorted_alphanumeric_list, sorted_children_dict
+)
+from .locations import get_locations, filter_xy
+from .numerical import (
+    find_curves, running_mean, traveling_salesman, walk_edges,
+    weighted_average, active_from_xyz, truncate_locs_depths,
+    minimum_depth_core,
+)
+from .plotting import symlog, inv_symlog, format_axis
+from .transformations import rotate_xyz
+from .workspace import get_output_workspace
--- geoapps_utils/locations.py
+++ geoapps_utils/locations.py
@@ -8,6 +8,7 @@
 from __future__ import annotations
 
 from uuid import UUID
+import numpy as np
 
 from geoh5py import Workspace
 from geoh5py.data import Data
@@ -47,3 +48,75 @@ def get_locations(workspace: Workspace, entity: UUID | Points | GridObject | Dat
         locations = get_locations(workspace, entity_obj.parent)
 
     return locations
+
+def filter_xy(
+    x: np.array,
+    y: np.array,
+    distance: float | None = None,
+    window: dict | None = None,
+    angle: float | None = None,
+    mask: np.ndarray | None = None,
+) -> np.array:
+    """
+    Window and down-sample locations based on distance and window parameters.
+
+    :param x: Easting coordinates, as vector or meshgrid-like array
+    :param y: Northing coordinates, as vector or meshgrid-like array
+    :param distance: Desired coordinate spacing.
+    :param window: Window parameters describing a domain of interest.
+        Must contain the following keys and values:
+        window = {
+            "center": [X: float, Y: float],
+            "size": [width: float, height: float]
+        }
+        May also contain an "azimuth" in the case of rotated x and y.
+    :param angle: Angle through which the locations must be rotated
+        to take on a east-west, north-south orientation.  Supersedes
+        the 'azimuth' key/value pair in the window dictionary if it
+        exists.
+    :param mask: Boolean mask to be combined with filter_xy masks via
+        logical 'and' operation.
+
+    :return mask: Boolean mask to be applied input arrays x and y.
+    """
+
+    if mask is None:
+        mask = np.ones_like(x, dtype=bool)
+
+    azim = None
+    if angle is not None:
+        azim = angle
+    elif window is not None:
+        if "azimuth" in window:
+            azim = window["azimuth"]
+
+    is_rotated = False if (azim is None) | (azim == 0) else True
+    if is_rotated:
+        xy_locs = rotate_xyz(np.c_[x.ravel(), y.ravel()], window["center"], azim)
+        xr = xy_locs[:, 0].reshape(x.shape)
+        yr = xy_locs[:, 1].reshape(y.shape)
+
+    if window is not None:
+        if is_rotated:
+            mask, _, _ = window_xy(xr, yr, window, mask=mask)
+        else:
+            mask, _, _ = window_xy(x, y, window, mask=mask)
+
+    if distance not in [None, 0]:
+        is_grid = False
+        if x.ndim > 1:
+            if is_rotated:
+                u_diff = np.unique(np.round(np.diff(xr, axis=1), 8))
+                v_diff = np.unique(np.round(np.diff(yr, axis=0), 8))
+            else:
+                u_diff = np.unique(np.round(np.diff(x, axis=1), 8))
+                v_diff = np.unique(np.round(np.diff(y, axis=0), 8))
+
+            is_grid = (len(u_diff) == 1) & (len(v_diff) == 1)
+
+        if is_grid:
+            mask, _, _ = downsample_grid(x, y, distance, mask=mask)
+        else:
+            mask, _, _ = downsample_xy(x, y, distance, mask=mask)
+
+    return mask
