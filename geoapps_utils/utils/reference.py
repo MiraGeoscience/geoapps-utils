1656dc2db9c405a0bc16b2c024f7fd3097c0cb2a
MatthieuCMira <109624972+MatthieuCMira@users.noreply.github.com>
2024-11-12 09:52:32 -0500

    removing the functions as I'm not using them and create a specific function for my need

--- geoapps_utils/utils/reference.py
+++ geoapps_utils/utils/reference.py
@@ -10,66 +10,22 @@ from __future__ import annotations
 from typing import Any
 from uuid import UUID
 
-from geoh5py.groups import PropertyGroup
-from geoh5py.shared import Entity
 from geoh5py.workspace import Workspace
 
 
-def get_entities(
-    workspace: Workspace,
-    entities: str
-    | UUID
-    | Entity
-    | PropertyGroup
-    | list[str | UUID | Entity | PropertyGroup],
-) -> list[Entity]:
+def get_name_from_uid(workspace: Workspace, uid: Any) -> str:
     """
-    Get a list of entities from a workspace.
+    Get a name from a uid.
 
-    :param workspace: Workspace containing the entities.
-    :param entities: Entity or list of entities to get.
+    :param workspace: The workspace to search the UUID in.
+    :param uid: The UUID to extract a name from.
 
-    :return: a list of entities.
+    :return: the name
     """
-
-    if not isinstance(entities, list):
-        entities = [entities]
-
-    out_list = []
-    for entity in entities:
-        if isinstance(entity, str | UUID):
-            temp = workspace.get_entity(entity)
-            if len(temp) > 1:
-                raise ValueError(f"Multiple ({len(temp)}) entities found for {entity}.")
-            if len(temp) == 0 or temp[0] is None:
-                raise ValueError(f"No entity found for {entity}.")
-            entity = temp[0]
-        if not isinstance(entity, Entity):
-            raise TypeError(f"Entity {entity} is not a valid Entity.")
-        out_list.append(entity)
-
-    return out_list
-
-
-def get_a_from_b(entities: list[Entity], data_type: type, attr: str) -> list[Any]:
-    """
-    Get a list of attributes from a list of entities.
-
-    :param entities: List of entities.
-    :param data_type: The type of data to extract.
-    :param attr: The attribute to extract.
-
-    :return: A list of attributes.
-    """
-    if not isinstance(entities, list):
-        entities = [entities]
-
-    out_list = []
-    for entity in entities:
-        if not isinstance(entity, data_type):
-            raise TypeError(f"Entity {entity} is not a valid {data_type}.")
-        if not hasattr(entity, attr):
-            raise AttributeError(f"Attribute '{attr}' not found in {entity}.")
-        out_list.append(getattr(entity, attr))
-
-    return out_list
+    if isinstance(uid, str):
+        return uid
+    if isinstance(uid, UUID):
+        uid = workspace.get_entity(uid)[0]
+    if hasattr(uid, "name"):
+        return uid.name
+    raise AttributeError(f"No object with name corresponding to {uid}.")
