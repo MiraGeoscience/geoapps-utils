e871f734169b43223256073343727fe79e8a5bc9
MatthieuCMira <109624972+MatthieuCMira@users.noreply.github.com>
2024-12-10 16:07:51 -0500

    Update geoapps_utils/driver/driver.py
    Co-authored-by: domfournier <dominiquef@mirageoscience.com>

--- geoapps_utils/driver/driver.py
+++ geoapps_utils/driver/driver.py
@@ -117,7 +117,7 @@ class BaseDriver(ABC):
                 driver.run()
                 print(f"Results saved to {params.geoh5.h5file}")
             except GeoAppsError as error:
-                warn(f"Failed: {error}")
+                warn(f"ApplicationError: {error}")
 
         return driver
 
