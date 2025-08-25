6ae24fe8a8636661e3150a3c45901c96d587102d
saicheran <saicheranb@mirageoscience.com>
2025-08-25 13:16:14 -0400

    Implement dynamic versioning and update version handling across project files

--- geoapps_utils/__init__.py
+++ geoapps_utils/__init__.py
@@ -10,10 +10,18 @@
 
 from __future__ import annotations
 
-
-__version__ = "0.6.0a1"
-
+from importlib.metadata import PackageNotFoundError, version
 from pathlib import Path
+
+
+try:
+    from las_geoh5._version import __version__, __version_tuple__
+except ModuleNotFoundError:  # pragma: no cover
+    from datetime import datetime
+
+    __date_str = datetime.today().strftime("%Y%m%d")
+    __version__ = "0.0.0.dev0+" + __date_str
+    __version_tuple__ = (0, 0, 0, "dev0", __date_str)
 
 from geoapps_utils.utils import (
     conversions,
